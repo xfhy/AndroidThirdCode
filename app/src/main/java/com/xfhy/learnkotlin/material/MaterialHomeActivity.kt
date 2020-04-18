7479858615667e6ff0288b17c1643ad85f153e21
xfhy <xfhy666@qq.com>
2020-04-18 15:07:14 +0800

    Material: add toolbar

--- app/src/main/java/com/xfhy/learnkotlin/material/MaterialHomeActivity.kt
+++ app/src/main/java/com/xfhy/learnkotlin/material/MaterialHomeActivity.kt
@@ -2,12 +2,36 @@ package com.xfhy.learnkotlin.material
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.xfhy.learnkotlin.R
+import com.xfhy.learnkotlin.common.toast
+import kotlinx.android.synthetic.main.activity_material_home.*
 
 class MaterialHomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_material_home)
+
+        //让Toolbar保持与ActionBar的外观一致
+        setSupportActionBar(toolbar)
     }
+
+    //加载toolbar.xml菜单文件
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    //处理点击事件
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.ic_backup -> toast("备份")
+            R.id.ic_delete -> toast("删除")
+            R.id.ic_settings -> toast("设置")
+        }
+        return true
+    }
+
 }
